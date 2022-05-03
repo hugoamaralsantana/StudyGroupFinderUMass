981b5d3e90b7bcaae7b2ed36794ddb6ab1f14810
Victor Santana <victorsantana@1x-nat-vl930-172-30-70-254.wireless.umass.edu>
2022-05-03 17:44:08 -0400

    UI Layout Brainstorm

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,14 +2,29 @@ import MenuPage from './MenuPage';
 import StudyGroupListPage from './StudyGroupListPage';
 import NavBar from './NavBar'
 import { useState } from 'react';
+import styles from '../styles.scss'
 
 function Container(){
     const [currPage, setPage] = useState("studygrouplist");
     console.log(currPage);
     return(
-    <div className="h-100 w-100 d-flex position-absolute bg-primary px-4 py-3">
-        <div className="h-100 w-100 d-flex shadow rounded bg-secondary">
-            {(currPage === "studygrouplist" ? <StudyGroupListPage/> : <MenuPage/>)}
+    <div className="h-100 w-100 overflow-hidden d-flex position-absolute">
+        <div className="container-fluid bg-secondary p-0">
+            <div className="h-10 row">
+                
+            </div>
+            <div className="row">
+                    <div className="sidemenu col-4 bg-primary">
+
+                    </div>
+                <div className="col bg-danger">
+                    <button class="btn btn-warning menuCollapse" type="button" data-toggle="collapse" data-target="#collapseMenu" aria-expanded="false" aria-controls="collapseMenu">
+                        Collapse Menu
+                    </button>
+
+                    {(currPage === "studygrouplist" ? <StudyGroupListPage/> : <MenuPage/>)}
+                </div>
+            </div>
         </div>
     </div>
     );
